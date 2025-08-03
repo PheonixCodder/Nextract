fe00e44f1cb98c5baad5678d824e4f49ccd35dfa
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-08-04 00:19:34 +0500

    Setting up Workflows

---
+++ app/(dashboard)/workflows/_components/CreateWorkflowDialog.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+import { Dialog, DialogTrigger, DialogContent } from '@/components/ui/dialog'
+
+const CreateWorkflowDialog = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CreateWorkflowDialog
