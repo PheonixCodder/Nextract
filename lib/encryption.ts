18fdb25b46b7ab67c3a0eaec0aa6adf0054260e7
Abdulahad <abdulahadismail.dev@gmail.com>
2025-12-16 16:05:03 +0500

    Update encryption.ts

--- lib/encryption.ts
+++ lib/encryption.ts
@@ -1,29 +1,31 @@
-import crypto from 'crypto'
-import 'server-only'
+import crypto from "crypto";
+import "server-only";
 
-const ALG = 'aes-256-cbc'
+const ALG = "aes-256-cbc";
 
-export async function symmetricEncrypt(data:string){
-    const key = process.env.ENCRYPTION_KEY
-    if (!key) throw new Error('Encryption key is not set');
-
-    const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv(ALG, Buffer.from(key, 'hex'), iv);
-    let encrypted = cipher.update(data);
-
-    encrypted = Buffer.concat([encrypted, cipher.final()])
-    return iv.toString('hex') + ':' + encrypted.toString('hex')
+export function symmetricEncrypt(data: string) {
+  // openssl rand -hex 32
+  const key = process.env.ENCRYPTION_SECRET;
+  if (!key) {
+    throw new Error("encryption key is not set");
+  }
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(ALG, Buffer.from(key, "hex"), iv);
+  let encrypted = cipher.update(data);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return iv.toString("hex") + ":" + encrypted.toString("hex");
 }
 
 export const symmetricDecrypt = (encrypted: string) => {
-    const key = process.env.ENCRYPTION_KEY
-    if (!key) throw new Error('Encryption key is not set');
-
-    const textParts = encrypted.split(':')
-    const iv = Buffer.from(textParts.shift() as string, 'hex')
-    const encryptedText = Buffer.from(textParts.join(':'), 'hex')
-    const decipher = crypto.createDecipheriv(ALG, Buffer.from(key, 'hex'), iv )
-    let decrypted = decipher.update(encryptedText)
-    decrypted = Buffer.concat([decrypted, decipher.final()])
-    return decrypted.toString()
-}
+  const key = process.env.ENCRYPTION_SECRET;
+  if (!key) {
+    throw new Error("encryption key is not set");
+  }
+  const parts = encrypted.split(":");
+  const iv = Buffer.from(parts.shift() as string, "hex");
+  const encryptedText = Buffer.from(parts.join(":"), "hex");
+  const decipher = crypto.createDecipheriv(ALG, Buffer.from(key, "hex"), iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
+};
