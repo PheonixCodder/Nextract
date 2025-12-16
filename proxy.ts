836df8b460ce868c4cc9334507c0c1474e8da103
Abdulahad <abdulahadismail.dev@gmail.com>
2025-12-16 15:47:04 +0500

    Shift to Next 16

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/api/workflows(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/api/workflows(.*)',   '/api/webhooks(.*)',])
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
