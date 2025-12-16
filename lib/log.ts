f5b48b3592a70819e51e09a411726bd9ada95ada
Abdulahad <abdulahadismail.dev@gmail.com>
2025-12-16 16:07:02 +0500

    Update Log Functionality

--- lib/log.ts
+++ lib/log.ts
@@ -1,16 +1,26 @@
-import { Log, LogCollector, LogFunction, LogLevel, LogLevels } from "@/types/log";
+import {
+  Log,
+  LogCollector,
+  LogFunction,
+  LogLevel,
+  LogLevels,
+} from "@/types/log";
 
 export function createLogCollector(): LogCollector {
   const logs: Log[] = [];
+
   const getAll = () => logs;
 
   const logFunctions = {} as Record<LogLevel, LogFunction>;
-  LogLevels.forEach(
-    (level) =>
-      (logFunctions[level] = (message: string) => {
-        logs.push({ message, level, timestamp: new Date() });
-      })
-  );
+  LogLevels.forEach((level) => {
+    logFunctions[level] = (message: string) => {
+      logs.push({
+        message,
+        level,
+        timestamp: new Date(),
+      });
+    };
+  });
   return {
     getAll,
     ...logFunctions,
